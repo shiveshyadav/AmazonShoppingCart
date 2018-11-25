efdbfe0c93e748ea5636d1ec97bf15d6dafa1f97
unknown <shiveshyadav@gmail.com>
2018-11-25 18:03:56 +0530

    config file update

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -5,16 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-
-
-
 public abstract class TestBase {
-	public static Properties env=null;
-	public static Properties config=null;
-	public static Properties data=null;
 	private static boolean isInitalized=false;
 	public static WebDriver driver = null;
 	public static WebDriverWait wait = null;
@@ -30,12 +23,10 @@ public abstract class TestBase {
 			ChromeOptions options = new ChromeOptions();
 			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
 			options.addArguments("--start-maximized");
-			options.addArguments("--lang=es");
 			driver = new ChromeDriver(options);
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			
-			
-		}
+					
+	}
 	
 	
 	@AfterSuite(alwaysRun = true)
